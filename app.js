0c35c64189bc5e6a4fece69a258377f5534d8175
Nassim Amar <nassimamar@hotmail.com>
2017-03-20 08:49:25 +0000

    random tokens added

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var connect = require('connect');
 var http = require('http');
 var url  = require('url');
+var crypto = require('crypto');
 
 var app = connect();
+
+var DEFAULT_TOKEN_SIZE = 16
+
+var isDebug = true;
 
 // gzip/deflate outgoing responses
 var compression = require('compression');
@@ -12,15 +17,33 @@ app.use(compression());
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/tokenFor', function (req, res, next) {
+app.use('/token', function (req, res, next) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
 
-    console.log(query); //{Object}
+    DebugPrint(query); //{Object}
 
-    res.end("End");
+    crypto.randomBytes( query["size"] > 0? parseInt( query[ "size" ] ) : DEFAULT_TOKEN_SIZE, function( err, buffer ) {
+        var token = buffer.toString( "hex" );
+            res.end( token );
+    } );
 
 })
 
 //create node.js http server and listen on port
 http.createServer(app).listen(3000);
+
+/********************* Helpers ****************************/
+
+// crypto hashing
+function sha256( data )
+{
+    return crypto.createHash('sha256').update( data ).digest( "base64" );
+}
+/********************* Debug ****************************/
+
+function DebugPrint( arg )
+{
+    if( !isDebug ) return;
+    console.log( arg );
+}
